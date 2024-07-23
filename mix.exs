527160659e8a2e8531d40500799f6619e193a15a
William Huba <william@jumpwire.io>
2024-07-23 11:26:31 -0400

    Set metadata for publishing

--- mix.exs
+++ mix.exs
@@ -1,14 +1,23 @@
 defmodule UeberauthWorkosAuthkit.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/jumpwire-ai/ueberauth_workos_authkit"
+
   def project do
     [
       app: :ueberauth_workos_authkit,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      source_url: "https://github.com/jumpwire-ai/ueberauth_workos_authkit",
+
+      # Docs
+      name: "Ueberauth WorkOS AuthKit",
+      description: description(),
+      source_url: @source_url,
+      package: package(),
+      docs: [extras: ["README.md", "LICENSE"], source_ref: @version]
     ]
   end
 
@@ -28,6 +37,18 @@ defmodule UeberauthWorkosAuthkit.MixProject do
       {:jose, "~> 1.11"},
 
       {:mock, "~> 0.3", only: :test},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+    ]
+  end
+
+  defp description do
+    "Implementation of an Ueberauth Strategy for WorkOS Single Sign-On with managed users."
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url},
     ]
   end
 end
