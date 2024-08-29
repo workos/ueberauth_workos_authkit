11bf05c80625c622299fc5fd40a3fbf2f891f17f
William Huba <william@jumpwire.ai>
2024-08-29 16:24:57 -0400

    Relax required Elixir version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule UeberauthWorkosAuthkit.MixProject do
     [
       app: :ueberauth_workos_authkit,
       version: @version,
-      elixir: "~> 1.16",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
